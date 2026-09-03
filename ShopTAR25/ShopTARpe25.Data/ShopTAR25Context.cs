42cf61124023b48cb710aedaf0d8589f5b1f09de
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-03 14:03:15 +0300

    Shop

---
+++ ShopTAR25/ShopTARpe25.Data/ShopTAR25Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopTARpe25.Data
+{
+    //Teha sellest classist DbContext, et saaks andmebaasi kasutada
+    public class ShopTAR25Context : DbContext
+    {
+        public ShopTAR25Context(DbContextOptions<ShopTAR25Context> options)
+            : base(options)
+        {
+        }
+        //teha Dbset, et saaks andmebaasi kasutada
+        //teha Core projekti alla Domain nimega kaust ja sinna class
+        //nimega Spaceship
+       
+    }
+}
