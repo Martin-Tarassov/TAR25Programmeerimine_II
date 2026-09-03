9ccc0687af6aa6a9509c8c77ce83b01ad84d70e3
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-03 14:13:57 +0300

    Spaceship

--- ShopTAR25/ShopTARpe25.Core/Domain/Spaceship.cs
+++ ShopTAR25/ShopTARpe25.Core/Domain/Spaceship.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace ShopTARpe25.Core.Domain
 {
-    internal class Spaceship
+    public class Spaceship
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Classification { get; set; } = string.Empty;
+        public DateTime? BuiltDate { get; set; }
+        public int? Crew { get; set; }
+        public int? EnginePower { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+
     }
+
 }
